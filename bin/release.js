88c4c7832daae34c06735ac23ca1dd7d3c617f60
xn <280145668@qq.com>
2023-04-28 08:07:38 +0800

    x

---
+++ bin/release.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+require("shelljs/global");
+// var exec = require('child_process').exec;
+var path = require("path");
+var { version } = require("../package.json");
+
+var keypath = path
+  .resolve("./conf/private.wxcec88a7e2c1e81c7.key")
+  .replaceAll("\\", "/");
+var description = process.argv[2] || "nothing";
+var cmd = `cli publish --platform mp-weixin --project jaqn-mini --upload true --appid wxcec88a7e2c1e81c7 --privatekey ${keypath} --description "${description}" --version ${version}`;
+console.log("路径:", cmd);
+
+exec(cmd, function (err, stdout, stderr) {
+  if (err) {
+    console.log("seems err:", err, { stdout, stderr });
+    throw new Error(err);
+  }
+  console.log("stdout", JSON.stringify(stdout));
+});
