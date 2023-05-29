cb8ec7ab9ebc5b14e2e1bf4c80efe69c1a404947
xiangnan <280145668@qq.com>
2023-05-29 17:23:46 +0800

    x

--- bin/git.py
+++ bin/git.py
@@ -42,4 +42,15 @@ if args.production:
     exec(f"git push p master {migrate_token};")
 
 if args.github:
-    exec("git push origin master" )
+    exec('''while true
+do
+  git push origin master
+  if [ $? -eq 0 ]
+  then
+    echo "Git push succeeded"
+    break
+  else
+    echo "Git push failed, retrying in 1 seconds"
+    sleep 1
+  fi
+done''')
