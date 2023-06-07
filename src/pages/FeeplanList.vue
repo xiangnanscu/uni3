f7df84de13a9e6049187ebe18a0a718a1f04c069
xiangnan <280145668@qq.com>
2023-06-07 16:54:29 +0800

    团费支付

--- src/pages/FeeplanList.vue
+++ src/pages/FeeplanList.vue
@@ -1,50 +1,100 @@
 <template>
-  <page-layout>
+  <uni-card :title="branch_name" :extra="member.xm">
     <uni-list :border="false">
       <uni-list-item
         v-for="(item, index) in FeeplanList"
         :key="index"
-        :to="`/pages/FeeplanDetail?id=${item.id}`"
         :showArrow="false"
-        :title="item.title"
-        :rightText="fromNow(item.ctime)"
+        :title="`${item.year}年${String(item.month).padStart(2, '0')}月团费${
+          item.amount
+        }元`"
       >
+        <template #footer>
+          <button
+            v-if="item.status !== '已缴费'"
+            :disabled="disabled"
+            @click="payFee(item)"
+            type="primary"
+            size="mini"
+          >
+            缴费
+          </button>
+          <span v-else>{{ item.status }}</span>
+        </template>
       </uni-list-item>
     </uni-list>
-    <uni-pagination :total="total" @change="clickPage" :current="current" />
-  </page-layout>
+  </uni-card>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      pageSize: 10,
-      total: 0,
-      current: 1,
-      query: {},
+      disabled: false,
+      member: null,
+      branch_name: "",
       FeeplanList: []
     };
   },
   async onLoad(query) {
-    this.query = query;
     await this.fetchData(query);
   },
   methods: {
-    async clickPage(e) {
-      this.current = e.current;
-      await this.fetchData({ page: this.current, pagesize: this.pageSize });
-    },
     async fetchData(query) {
-      const {
-        data: { records, total }
-      } = await Http.get(
-        `/feeplan?page=${query.page || this.current}&pagesize=${
-          query.pagesize || this.pageSize
-        }`
-      );
+      const { data: records } = await Http.post(`/youth_fee/self`);
+      const { data: member } = await Http.post(`/youth_member/get`, {
+        sfzh: this.user.username
+      });
+      this.member = member;
+      this.branch_name = member.branch_name.replace("四川省宜宾市江安县", "");
       this.FeeplanList = records;
-      this.total = total;
+    },
+    async payFee({ feeplan_id: planId }) {
+      const { data: payed } = await Http.post(`/orders/check`, {
+        feeplan_id: planId
+      });
+      console.log({ payed });
+      if (payed) {
+        return uni.showToast({
+          title: `您已缴费, 无需重复`,
+          icon: "none"
+        });
+      }
+      this.disabled = true;
+      const { code, errMsg } = await uni.login();
+      if (errMsg !== "login:ok") {
+        throw new Error(errMsg);
+      }
+      const { data } = await Http.post(`/wx/jsapi_mini_preorder`, {
+        code,
+        feeplan_id: planId
+      });
+      const {
+        signature: paySign,
+        prepay_id,
+        timestamp: timeStamp,
+        nonce_str: nonceStr
+      } = data;
+      const self = this;
+      const opts = {
+        timeStamp: String(timeStamp),
+        nonceStr: String(nonceStr),
+        package: `prepay_id=${prepay_id}`,
+        signType: "RSA",
+        paySign: paySign,
+        async success(res) {
+          self.disabled = false;
+          if (res.errMsg == "requestPayment:ok") {
+            uni.showToast({ title: "缴费成功" });
+          }
+        },
+        fail(res) {
+          self.disabled = false;
+          console.log("wx.requestPayment fail:", res);
+        }
+      };
+      wx.requestPayment(opts);
+      // await utils.tryGotoPage();
     }
   }
 };
