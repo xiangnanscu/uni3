37f3333388dc0ac71df113c93c7bafbfdfc57aea
xn <280145668@qq.com>
2023-06-18 01:28:54 +0800

    feat:分享到朋友圈

--- src/pages/NewsDetail.vue
+++ src/pages/NewsDetail.vue
@@ -3,7 +3,7 @@
     <uni-card v-if="news" :isFull="true" :is-shadow="false" :border="false">
       <p class="news-title">{{ news.title }}</p>
       <tinymce-text :html="news.content"></tinymce-text>
-      <template #actions> 分享 </template>
+      <template #actions> </template>
     </uni-card>
   </page-layout>
 </template>
@@ -12,13 +12,22 @@
 export default {
   data() {
     return {
-      query: {},
       news: null
     };
   },
   async onLoad(query) {
     this.query = query;
     await this.fetchData(query);
+  },
+  onShareTimeline(options) {
+    console.log(options);
+    const page = getCurrentPages().slice(-1)[0];
+    console.log({ page });
+    return {
+      title: this.news.title,
+      path: page.$page.fullPath,
+      imageUrl: ""
+    };
   },
   onShareAppMessage(options) {
     console.log(options);
@@ -31,6 +40,9 @@ export default {
     };
   },
   methods: {
+    onTap(event) {
+      console.log(event);
+    },
     async fetchData(query) {
       const { data: news } = await Http.get(`/news/detail/${query.id}`);
       this.news = news;
