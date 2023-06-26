49725b6f32609a3ef3be9f2571050f0f80a01209
xn-hy <280145668@qq.com>
2023-06-26 19:18:57 +0800

    添加好友测试3

--- src/views/MyQrCode.vue
+++ src/views/MyQrCode.vue
@@ -1,14 +1,27 @@
 <template>
-  <page-layout> OK </page-layout>
+  <page-layout>
+    <div>
+      <uni-list :border="false">
+        <uni-list-chat
+          :avatar-circle="true"
+          :title="approver.nickname"
+          :avatar="approver.avatar"
+          :note="approver.intro"
+        ></uni-list-chat>
+      </uni-list>
+    </div>
+  </page-layout>
 </template>
 
 <script setup>
-const user = useUser();
-const query = ref();
+const approver = ref();
+const ready = ref();
 onLoad(async (opts) => {
   console.log("onLoad my_qr_code", opts);
   const uid = decodeURIComponent(opts.q).split("/").pop();
   const res = await usePost(`/friends/apply/${uid}`);
+  approver.value = res.approve;
+  ready.value = true;
   console.log({ uid, res });
 });
 </script>
