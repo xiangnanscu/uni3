1310a47ce75f0ea02b125617c0269a0002d4b97d
xn <280145668@qq.com>
2023-07-13 08:20:02 +0800

    x

--- bin/wx-release.js
+++ bin/wx-release.js
@@ -2,6 +2,7 @@
 
 require("shelljs/global");
 const os = require("os");
+const ci = require("miniprogram-ci");
 
 // var exec = require('child_process').exec;
 var path = require("path");
@@ -18,16 +19,33 @@ if (os.type() === "1Windows_NT") {
   cmd = `miniprogram-ci upload --pp ./dist/build/mp-weixin --pkp ./conf/private.wxcec88a7e2c1e81c7.key --appid wxcec88a7e2c1e81c7 -r 1  --uv ${version}`;
 }
 
-console.log("路径:", cmd);
+console.log();
+const project = new ci.Project({
+  appid: "wxcec88a7e2c1e81c7",
+  type: "miniProgram",
+  projectPath: "./dist/build/mp-weixin",
+  privateKeyPath: "./conf/private.wxcec88a7e2c1e81c7.key",
+  ignores: ["node_modules/**/*"]
+});
+
+ci.upload({
+  project,
+  version: version,
+  desc: process.argv[2] || "update",
+  setting: {
+    es6: false,
+    minify: true
+  }
+});
 
 // eslint-disable-next-line no-undef
-exec(cmd, function (err, stdout, stderr) {
-  if (err) {
-    console.log("seems err:", err, {
-      stdout,
-      stderr
-    });
-    throw new Error(err);
-  }
-  console.log("stdout", JSON.stringify(stdout));
-});
+// exec(cmd, function (err, stdout, stderr) {
+//   if (err) {
+//     console.log("seems err:", err, {
+//       stdout,
+//       stderr
+//     });
+//     throw new Error(err);
+//   }
+//   console.log("stdout", JSON.stringify(stdout));
+// });
