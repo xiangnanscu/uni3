bb703add008d76920cd4bc431642760385935879
xiangnan <280145668@qq.com>
2023-07-25 17:47:59 +0800

    feat:用户创建男女神

--- src/views/GoddessAdd.vue
+++ src/views/GoddessAdd.vue
@@ -1,104 +1,27 @@
 <template>
   <page-layout>
-    <uni-forms
-      ref="valiForm"
-      validateTrigger="blur"
-      :rules="formRules"
-      :model-value="GoddessAddData"
-      label-position="top"
-    >
-      <uni-forms-item label="女神宣言1" name="content">
-        <uni-easyinput
-          v-model="GoddessAddData.content"
-          type="textarea"
-          autoHeight
-        />
-      </uni-forms-item>
-      <uni-forms-item label="图片" name="pics">
-        <alioss-picker
-          @fail="uploadFail"
-          limit="9"
-          v-model="GoddessAddData.pics"
-          oss-size="100k"
-        ></alioss-picker>
-      </uni-forms-item>
-    </uni-forms>
-    <x-button @click="submit('valiForm')">提交</x-button>
+    <modelform-uni
+      :model="GoddessAddModel"
+      action-url="/goddess/create_from_user"
+    ></modelform-uni>
   </page-layout>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      params: {},
-      GoddessAddData: {
-        content: "",
-        pics: []
-      },
-      formRules: {
-        content: {
-          rules: [
-            {
-              required: true,
-              errorMessage: "女神宣言不能为空"
-            },
-            {
-              validateFunction: function (rule, value, data, callback) {
-                if (value.length > 1000) {
-                  callback("字数不能超过1000");
-                }
-                return true;
-              }
-            }
-          ]
-        },
-        pics: {
-          rules: [
-            {
-              validateFunction: function (rule, value, data, callback) {
-                if (value.length === 0) {
-                  callback("至少要上传一张图片");
-                }
-                return true;
-              }
-            }
-          ]
-        }
-      }
-    };
-  },
-  onReady() {
-    // 需要在onReady中设置规则
-    this.$refs.valiForm.setRules(this.formRules);
-  },
-  async onLoad(params) {
-    this.params = params;
-    // await this.fetchData(params);
-  },
-
-  methods: {
-    uploadFail({ tempFiles, tempFilePaths }) {
-      console.log({
-        tempFiles,
-        tempFilePaths
-      });
-      console.log(this.GoddessAddData.pics);
-    },
-    async fetchData(params) {
-      const { data } = await Http.get(`/GoddessAdd/${params.id}`);
-      this.GoddessAddData = data;
-    },
-    async submit(ref) {
-      await this.$refs[ref].validate();
-      await Http.post(`/goddess/create`, {
-        content: this.GoddessAddData.content,
-        pics: this.GoddessAddData.pics.map((f) => f.url)
-      });
-      await utils.tryGotoPage();
+<script setup>
+const GoddessAddModel = Model.createModel({
+  fieldNames: ["sex", "title", "pics"],
+  fields: {
+    sex: { label: "性别", required: true, choices: ["男", "女"], tag: "radio" },
+    title: { label: "标题", required: true },
+    pics: {
+      label: "封面图",
+      type: "aliossImageList",
+      required: true,
+      maxCount: 1,
+      size: process.env.ALIOSS_AVATAR_SIZE || "2M"
     }
   }
-};
+});
 </script>
 
 <style scoped>
