fbbe6fe4deb936882d70416134ac1aecf04f1843
xiangnan <280145668@qq.com>
2023-08-18 00:11:05 +0800

    test:share-problem

--- src/views/MixinShare.js
+++ src/views/MixinShare.js
@@ -3,13 +3,16 @@ export default {
     return { record: null, shareTitlePrefix: "" };
   },
   onShareTimeline(options) {
+    console.log(this.shareData)
     return this.shareData;
   },
   onShareAppMessage(options) {
+    console.log(this.shareData)
     return this.shareData;
   },
   computed: {
     shareData() {
+      console.log(this.record)
       return {
         title: this.shareTitlePrefix + (this.record?.title || ""),
         desc: this.record?.content.slice(0, 20) + "...",
