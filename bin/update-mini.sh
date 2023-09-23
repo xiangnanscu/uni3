f7550965b1e579179a117a56e37fdd4be6934695
xiangnan <280145668@qq.com>
2023-09-23 19:32:29 +0800

    format:field.mjs

--- bin/update-mini.sh
+++ bin/update-mini.sh
@@ -2,4 +2,6 @@ rm -rf xodel
 gh repo clone ${app:-jaqn} xodel -- --depth=1
 rm -rf src/lib/model
 cp -r xodel/src/lib/model src/lib
+cp xodel/.vscode/settings.json .vscode
+cp xodel/.eslintrc.cjs .
 rm -rf xodel
