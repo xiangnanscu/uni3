3f9e712b6550b302adda0ea1deadfd9394550a36
xiangnan <280145668@qq.com>
2023-09-23 15:49:33 +0800

    update-script

--- bin/update-mini.sh
+++ bin/update-mini.sh
@@ -1,6 +1,5 @@
 rm -rf xodel
 gh repo clone ${app:-jaqn} xodel -- --depth=1
-cp -r xodel/src/lib/field.mjs src/lib
-cp -r xodel/src/lib/model.mjs src/lib
-cp -r xodel/src/lib/validator.mjs src/lib
+rm -rf src/lib/model
+cp -r xodel/src/lib/model src/lib
 rm -rf xodel
