9e852b2141c40aef4fd14d637114773129fa8bb9
xiangnan <280145668@qq.com>
2023-10-08 11:57:13 +0800

    add-search

--- src/views/SchoolStudentStat.vue
+++ src/views/SchoolStudentStat.vue
@@ -1,5 +1,13 @@
 <template>
   <page-layout>
+    <view class="search-bar">
+      <uni-easyinput
+        suffixIcon="search"
+        v-model="searchValue"
+        placeholder="请输入查找内容."
+        iconClick="onClick"
+      ></uni-easyinput>
+    </view>
     <x-title>学生统计情况（{{ students.length }}人）</x-title>
     <div v-if="students.length">
       <template v-for="[grade, classes] of Object.entries(grades)" :key="grade">
@@ -25,6 +33,7 @@
 <script setup>
 const students = ref([]);
 const grades = reactive({});
+const searchValue = ref("");
 onLoad(async () => {
   students.value = await usePost(`/student/records?select=grade&select=class`);
   for (const s of students.value) {
