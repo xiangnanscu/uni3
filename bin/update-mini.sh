71654348641a369662d6693eda828db082276226
xiangnan <280145668@qq.com>
2023-10-21 20:14:42 +0800

    x

--- bin/update-mini.sh
+++ bin/update-mini.sh
@@ -1,5 +1,5 @@
 rm -rf xodel
-gh repo clone ${app:-jaqn} xodel -- --depth=1
+gh repo clone ${app:-xodel} xodel -- --depth=1
 rm -rf src/lib/model
 cp -r xodel/src/lib/model src/lib
 cp xodel/.vscode/settings.json .vscode
