58727e7e83115d0bb5447d2737cfa6b12611dd44
xiangnan <280145668@qq.com>
2023-10-23 02:33:42 +0800

    test:useRedirect使用ref

--- src/composables/useRedirect.js
+++ src/composables/useRedirect.js
@@ -1,15 +1,10 @@
-export function useRedirect(query) {
-  const redirect = computed(() => {
+export function useRedirect(query = useQuery()) {
+  const redirect = ref();
+  onLoad(() => {
     if (query.redirect) {
-      const redirectUrl = query.redirect;
-      if (redirectUrl.startsWith(process.env.UNI_LOGIN_PAGE)) {
-        // 10.22因为未注册用户点击别人分享的需要实名的页面时, 需要先重定向到登录, 再重定向到实名,再重定向到最终页面
-        // 所以这里不能绕过登录页面
-        // redirectUrl = process.env.UNI_HOME_PAGE;
-      }
-      return decodeURIComponent(redirectUrl);
+      redirect.value = decodeURIComponent(query.redirect);
     } else {
-      return process.env.UNI_HOME_PAGE;
+      redirect.value = process.env.UNI_HOME_PAGE;
     }
   });
   return redirect;
