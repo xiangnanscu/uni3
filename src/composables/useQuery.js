22488ef951a4f7e290c555d2936ec61243ba9894
xiangnan <280145668@qq.com>
2023-10-23 00:43:47 +0800

    test:需要实名和登录的逻辑修改

--- src/composables/useQuery.js
+++ src/composables/useQuery.js
@@ -3,6 +3,7 @@ export function useQuery() {
   onLoad((options) => {
     for (const [name, value] of Object.entries(options)) {
       query[name] = decodeURIComponent(value);
+      console.log(`useQuery 解码 ${name}: ${value} => ${query[name]}`);
     }
   });
   return query;
