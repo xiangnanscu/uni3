4919bee275cd00334bc2c3c529d764fe28076acc
xiangnan <280145668@qq.com>
2023-11-20 17:55:24 +0800

    入住按钮

--- src/views/StageDetail.vue
+++ src/views/StageDetail.vue
@@ -12,10 +12,11 @@
         style="width: 100%"
       />
       <fui-preview :previewData="previewData"></fui-preview>
+      <x-button @click="joinVol" :plain="false" size="default">申请入住</x-button>
       <tinymce-text :html="record.content" style="margin-top: 1em"></tinymce-text>
       <template #actions> </template>
     </uni-card>
-    <x-button @click="joinVol" type="default" :plain="false">申请入住</x-button>
+    <x-button @click="joinVol" :plain="false" size="default">申请入住</x-button>
     <div style="height: 4em"></div>
     <x-bottom>
       <generic-actions :target="record" target-model="stage" style="width: 100%" />
