a9d9f5c6c3c3d4efa69477dc6761dae9fa36819c
xiangnan <280145668@qq.com>
2023-11-27 10:12:56 +0800

    转盘文本优化

--- src/lib/utils.mjs
+++ src/lib/utils.mjs
@@ -381,3 +381,13 @@ export function timeParser(t) {
     throw new Error("invalid type:" + typeof t);
   }
 }
+export function getLocaltime(date = new Date()) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
