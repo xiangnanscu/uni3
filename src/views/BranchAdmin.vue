47a659522f4d0ea54cc46ddad384e85e217a6649
xiangnan <280145668@qq.com>
2023-12-14 10:33:53 +0800

    feat:微信扫码登录

---
+++ src/views/BranchAdmin.vue
@@ -0,0 +1,59 @@
+<template>
+  <page-layout>
+    <x-title>支部管理</x-title>
+    <div v-if="ready">
+      <uni-list :border="false">
+        <uni-list-item
+          v-if="sysadminRole"
+          title="邀请团县委管理员"
+          thumb-size="lg"
+          link
+          to="/views/BranchRegeditPrincipal"
+          :showArrow="false"
+        />
+        <uni-list-item
+          v-if="sysadminRole"
+          title="审核团县委管理员"
+          thumb-size="lg"
+          link
+          to="/views/BranchCheckPrincipal"
+          :showArrow="false"
+        />
+        <uni-list-item
+          v-if="sysadminRole || principalRole"
+          title="邀请乡镇管理员"
+          thumb-size="lg"
+          link
+          to="/views/SchoolRegeditClassDirector"
+          :showArrow="false"
+        />
+        <uni-list-item
+          v-if="sysadminRole || principalRole"
+          title="审核乡镇管理员"
+          thumb-size="lg"
+          link
+          to="/views/SchoolCheckClassDirector"
+          :showArrow="false"
+        />
+      </uni-list>
+    </div>
+    <x-alert v-else title="没有记录"> </x-alert>
+  </page-layout>
+</template>
+
+<script setup>
+const ready = ref(false);
+const { session } = useSession();
+const user = session.user;
+const query = useQuery();
+const principalRole = ref();
+const sysadminRole = ref();
+onLoad(async () => {
+  const roles = await helpers.getPassedRoles();
+  sysadminRole.value = roles.sys_admin;
+  principalRole.value = roles.branch_principal;
+  ready.value = true;
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/MiniQrcodeDispatcher.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>跳转中...</div>
+</template>
+
+<script setup>
+const loginPage = process.env.UNI_LOGIN_PAGE;
+const user = useUser();
+const query = useQuery();
+onLoad(async (opts) => {
+  await utils.redirect(`/views/${query.q.split("/q/").pop()}`);
+});
+</script>
+
+<style scoped></style>
